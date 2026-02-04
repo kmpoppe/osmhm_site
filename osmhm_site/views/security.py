c3e8fd2c080eebab166c8abe4cd42a3bd5f8fb37
Kai Michael Poppe <kai@poppe.dev>
2026-02-04 15:49:44 +0000

    Rebuild for OAuth2

--- osmhm_site/views/security.py
+++ osmhm_site/views/security.py
@@ -1,3 +1,4 @@
+from requests_oauthlib import OAuth2Session
 from pyramid.view import view_config
 from xml.etree import ElementTree
 from pyramid.httpexceptions import HTTPFound, HTTPBadGateway, HTTPBadRequest
@@ -12,81 +13,83 @@ from pyramid.security import (
     forget,
 )
 
-# import urlparse
 from urllib.parse import parse_qsl
 import oauth2 as oauth
 
-CONSUMER_KEY = 'IMjqoNHVuMaGOkIggHhasrqg08GcXULWtMjs742i'
-CONSUMER_SECRET = '11tXVQCp50NpG3IBVo3R4fYIpjgbq24tL2FUuDXX'
+CLIENT_ID        = '4CydJZAh0ZwWYZ_i8vxHwgwGVHyCGw2xmZvOLElscto'
+CLIENT_SECRET    = 'xpzk51rLt5MOl97-vA1VYnbeOpVxxxAmkjEMP9nMYXI'
 
-BASE_URL = 'https://www.openstreetmap.org/oauth'
-REQUEST_TOKEN_URL = '%s/request_token' % BASE_URL
-ACCESS_TOKEN_URL = '%s/access_token' % BASE_URL
-AUTHORIZE_URL = '%s/authorize' % BASE_URL
+BASE_URL         = 'https://www.openstreetmap.org/oauth2'
+TOKEN_URL        = '%s/token' % BASE_URL
+AUTHORIZE_URL    = '%s/authorize' % BASE_URL
 
+SCOPE            = [ "read_prefs" ]
+REDIRECT_URI     = "https://172.29.164.150/oauth_callback"
 USER_DETAILS_URL = 'https://api.openstreetmap.org/api/0.6/user/details'
-
-consumer = oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
-
 
 @view_config(route_name='login')
 def login(request):
-    client = oauth.Client(consumer)
-    oauth_callback_url = request.route_url('oauth_callback')
-    url = "%s?oauth_callback=%s" % (REQUEST_TOKEN_URL, oauth_callback_url)
-    resp, content = client.request(url, "GET")
-    if resp['status'] != '200':
-        return HTTPBadGateway('The OSM authentication server didn\'t\
-                respond correctly')
-    request_token = dict(urlparse.parse_qsl(content))
-    session = request.session
-    session['request_token'] = request_token
-    session['came_from'] = request.params.get('came_from')
-    redirect_url = "%s?oauth_token=%s" % \
-                   (AUTHORIZE_URL, request_token['oauth_token'])
-    return HTTPFound(location=redirect_url)
 
+    osm = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+    authorization_url, state = osm.authorization_url(AUTHORIZE_URL)
+
+    # store state in session to validate callback
+    request.session['oauth_state'] = state
+    request.session['came_from'] = request.params.get('came_from')
+
+    return HTTPFound(location=authorization_url)
 
 @view_config(route_name='oauth_callback')
 def oauth_callback(request):
     session = request.session
-    request_token = session.get('request_token')
-    if request.params.get('oauth_token') != request_token['oauth_token']:
-        return HTTPBadRequest('Tokens don\'t match')
-    token = oauth.Token(request_token['oauth_token'],
-                        request_token['oauth_token_secret'])
-    verifier = request.params.get('oauth_verifier')
-    token.set_verifier(verifier)
-    client = oauth.Client(consumer, token)
-    resp, content = client.request(ACCESS_TOKEN_URL, "POST")
-    access_token = dict(urlparse.parse_qsl(content))
-    token = access_token['oauth_token']
-    token_secret = access_token['oauth_token_secret']
-    token = oauth.Token(token, token_secret)
-    client = oauth.Client(consumer, token)
-    resp, content = client.request(USER_DETAILS_URL, "GET")
-    user_elt = ElementTree.XML(content).find('user')
+    state = session.get('oauth_state')
 
-    if 'id' in user_elt.attrib:
-        userid = user_elt.attrib['id']
-        username = user_elt.attrib['display_name']
+    osm = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, state=state)
 
-        user = DBSession.query(User).get(userid)
-        if user is None:
-            user = User(userid, username)
-            DBSession.add(user)
-            DBSession.flush()
+    # Fetch the access token using the authorization code from the callback
+    callback_url = request.route_url(
+        'oauth_callback',
+        _scheme='https',
+        _port=443
+    ) + '?' + request.query_string
 
-        if DBSession.query(User).filter(User.role == User.role_owner) \
-                    .count() == 0:
-            user = DBSession.query(User).get(userid)
-            user.role = User.role_owner
+    token = osm.fetch_token(
+        TOKEN_URL,
+        client_secret=CLIENT_SECRET,
+        authorization_response=callback_url
+    )
 
-        headers = remember(request, userid, max_age=20 * 7 * 24 * 60 * 60)
+    access_token = token.get('access_token')
+    if not access_token:
+        return HTTPBadRequest('Failed to obtain access token from OSM')
 
+    # Use Bearer token to fetch user details
+    headers = {'Authorization': f'Bearer {access_token}'}
+    resp = osm.get(USER_DETAILS_URL, headers=headers)
+    user_elt = ElementTree.fromstring(resp.content).find('user')
+
+    if 'id' not in user_elt.attrib:
+        return HTTPBadRequest('Failed to retrieve user information from OSM')
+
+    userid = user_elt.attrib['id']
+    username = user_elt.attrib['display_name']
+
+    # ======== Database logic (mostly unchanged) ========
+    user = DBSession.query(User).get(userid)
+    if user is None:
+        user = User(userid, username)
+        DBSession.add(user)
+        DBSession.flush()
+
+    # Ensure the first user becomes owner
+    if DBSession.query(User).filter(User.role == User.role_owner).count() == 0:
+        user.role = User.role_owner
+
+    # Set session cookie
+    headers = remember(request, userid, max_age=20 * 7 * 24 * 60 * 60)
     location = session.get('came_from') or request.route_path('home')
-    return HTTPFound(location=location, headers=headers)
 
+    return HTTPFound(location=location, headers=headers)
 
 @view_config(route_name='logout')
 def logout(request):  # pragma: no cover
